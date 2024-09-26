6e21415a1ec22fea1944d4f2b1cfba065737305a
Yajme <spartan.emjayrongavilla@gmail.com>
2024-09-26 20:35:37 +0800

    deploy server to vercel

--- controller/mapController.js
+++ controller/mapController.js
@@ -4,37 +4,35 @@ import axios from 'axios';
 dotenv.config();
 const apiKey = process.env.OPEN_ROUTE_SERVICE_APIKEY;
 const baseURL = 'https://api.openrouteservice.org/v2/directions/driving-car?';
-const requestDirection = async (startingPoint,endPoint)=>{
+const requestDirection = async (startingPoint, endPoint) => {
 
-const request =`${baseURL}api_key=${apiKey}&start=${startingPoint}&end=${endPoint}`;
+	const request = `${baseURL}api_key=${apiKey}&start=${startingPoint}&end=${endPoint}`;
+	try {
 
+		const response = await axios.get(request);
 
-try{
-
-const response = await axios.get(request);
-
-return response;
-}catch(error){
-throw error;
-}
+		return response;
+	} catch (error) {
+		throw error;
+	}
 };
 
 
-const getDirection = async (req,res,next) =>{
-	try{
-	const startPoint = req.params.startPoint;
-	const endPoint = req.params.endPoint;
+const getDirection = async (req, res, next) => {
+	try {
+		const startPoint = req.params.startPoint;
+		const endPoint = req.params.endPoint;
 
-	const directions = await requestDirection(startPoint,endPoint);
+		const directions = await requestDirection(startPoint, endPoint);
 
-	res.json(directions.data);
+		res.json(directions.data);
 
-}catch(error){
-console.log(error);
-next(error);
+	} catch (error) {
+		console.log(error);
+		next(error);
+	}
 }
-}
 
 
 
-export default {getDirection};
+export default { getDirection };
