9694eae02c698c99fd0eeffc740771ad2da44ffa
Yajme <spartan.emjayrongavilla@gmail.com>
2024-09-28 20:38:51 +0800

    vercel troubleshooting (1)

--- routes/assets.js
+++ routes/assets.js
@@ -1,11 +1,33 @@
 import express from 'express';
-import path from 'path';
+import path from 'path';import fs from 'fs';
+
+
+// Function to list all directories synchronously
+const listDirectoriesSync = (dirPath) => {
+    let dir = '';
+  const files = fs.readdirSync(dirPath);
+
+  files.forEach((file) => {
+    const fullPath = path.join(dirPath, file);
+    
+    if (fs.statSync(fullPath).isDirectory()) {
+        dir += `${file} \n`;
+      console.log(file);  // This is a directory
+    }
+  });
+
+  return dir;
+};
+
+// Usage: Provide the directory path where you want to list folders
+
 const router = express.Router();
 
 const routerSetup = (projectRoot) => {
     router.use('/image', express.static(path.join(projectRoot, 'public/assets')));
     router.get('/test',(req,res)=>{
-        res.send(projectRoot);
+       const d = listDirectoriesSync(projectRoot); 
+        res.send(d);
     })
 
     
