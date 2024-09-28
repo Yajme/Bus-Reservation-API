12075682bc5c97c0db9619c508b5db3f4a415619
Yajme <spartan.emjayrongavilla@gmail.com>
2024-09-28 19:13:34 +0800

    Image cdn test

--- index.js
+++ index.js
@@ -2,11 +2,19 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import mapRoute from './routes/map.js';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+// Get the current file's directory name
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
+
+app.use(express.static(__dirname + '/public'));
 app.get("/",(request,response)=>{
 
 response.send("HELLO WORLD");
