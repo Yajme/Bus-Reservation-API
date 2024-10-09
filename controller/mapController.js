6453e089d15393677088423b5763a949456797a1
Emjay Rongavilla <88352665+Yajme@users.noreply.github.com>
2024-10-09 22:09:11 +0800

    Update mapController.js

--- controller/mapController.js
+++ controller/mapController.js
@@ -9,9 +9,7 @@ const baseURL = 'https://api.openrouteservice.org/';
 
 const autocomplete = async (req, res, next)=>{
 	try{
-		/* https://api.openrouteservice.org/geocode/autocomplete?api_key=5b3ce3597851110001cf6248069c18f57a1e40dd9ef9069ab99b8df3&text=Lipa&boundary.country=PH&layers=country,region,locality
-		https://api.openrouteservice.org/v2/geocode/autocomplete?api_key=5b3ce3597851110001cf6248069c18f57a1e40dd9ef9069ab99b8df3&text=Lipa&boundary.country=PH&layers=country,region,locality
-		*/
+		
 		const text = req.params.text;
 		const constraints = 'boundary.country=PH&layers=country,region,locality';
 		const request = `${baseURL}geocode/autocomplete?api_key=${apiKey}&text=${text}&${constraints}`;
